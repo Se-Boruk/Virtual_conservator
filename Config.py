c3361d5bff6f23e4459f5837e8ae6e0efd75f62e
Se-Boruk <Seb.Boruk@gmail.com>
2025-10-23 01:28:49 +0200

    DataBase manager
    Commit with the database manager.
    I also fixed gitignore file as it was .txt before.
    
    Replaced the downloading of database to database manager which has this functionality.
    
    Added global config file for our constants (like paths to important folders, or random sattes etc.)
    
    Also included my first part code inside auto_encoder which is my start for testing code and databases.

---
+++ Config.py
@@ -0,0 +1,25 @@
+#Config file with some important SYSTEM VARIABLES - to be constant across local repos and branches 
+#for reproducibility and easier work (so we have the same paths to files, to other classes, .py files etc.)
+
+import os
+
+#Project root (as config is in main repo folder)
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+#Path to the database folder
+DATABASE_FOLDER = os.path.join(PROJECT_ROOT, "DataBase")
+
+#Path to painintg dataset (for reconstruction)
+RECONSTRUCTION_DATASET_PATH = os.path.join(DATABASE_FOLDER, "WikiArt_reconstruction")
+
+#Name on hugging face (extraxted from link)
+# https://huggingface.co/datasets/Artificio/WikiArt
+RECONSTRUCTION_DATASET_NAME = "Artificio/WikiArt_Full"
+
+########################
+
+RANDOM_STATE = 111
+
+TRAIN_SPLIT = 0.7
+VAL_SPLIT = 0.15
+TEST_SPLIT = 0.15
